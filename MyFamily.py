0bcd1ee7c939b0cb2810a4399e7fae99e090f1be
hra <raadh791@gmail.com>
2023-01-17 11:08:34 +0100

    Le fichier Myfamily.py est ajouté

---
+++ MyFamily.py
@@ -0,0 +1,8 @@
+def my_function(fname):
+  print(fname + " Raad")
+print("My family is compsed from my wife Azraa El jawhari and the following children:")
+my_function("Batoul")
+my_function("Ali")
+my_function("Haidar")
+my_function("Nour")
+print("Thank my god")
